ff9009cb4b7a9413d52ff3e62c393cbee710096f
Adam Ciolkowski <adamciolkowski1@gmail.com>
2015-05-15 18:54:26 +0200

    Added base application logic

--- puzzle-solver.hs
+++ puzzle-solver.hs
@@ -4,7 +4,17 @@ readPuzzle path = do handle <- openFile path ReadMode
                      cols <- hGetLine handle
                      rows <- hGetLine handle
                      hClose handle
-                     return (toList cols, toList rows)
+                     printSolution (solvePuzzle (Puzzle (parse cols) (parse rows)))
+                     return ()
+					 where
+                       parse s = (read s)::[[Int]]
 
+data Puzzle = Puzzle [[Int]] [[Int]] deriving Show
+data Cell = Filled | Blank deriving Show
+data Solution = Solution [[Cell]] deriving Show
 
-toList s = (read s)::[[Int]]
+solvePuzzle :: Puzzle -> Solution
+solvePuzzle (Puzzle cols rows) = Solution [[]]	-- TODO
+
+printSolution :: Solution -> IO ()
+printSolution s = do return () 	-- TODO
