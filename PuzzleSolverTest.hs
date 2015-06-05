b30499fca50f681b48a7a897b3777a746d1b0396
Adam Ciolkowski <adamciolkowski1@gmail.com>
2015-06-05 14:12:16 +0200

    Added tests for PuzzleSolver

---
+++ PuzzleSolverTest.hs
@@ -0,0 +1,20 @@
+module PuzzleSolverTest where
+import Test.HUnit
+import Puzzle
+import PuzzleSolver
+
+allTests =
+ TestList
+  [
+   TestCase (assertEqual "hints: [0], length: 3" [[Blank, Blank, Blank]] (rowsForHint [0] 3)),
+   TestCase (assertEqual "hints: [3], length: 3" [[Filled, Filled, Filled]] (rowsForHint [3] 3)),
+   TestCase (assertEqual "hints: [1], length: 3" [[Filled, Blank, Blank],
+                                                  [Blank, Filled, Blank],
+                                                  [Blank, Blank, Filled]] (rowsForHint [1] 3)),
+   TestCase (assertEqual "hints: [1, 1], length: 3" [[Filled, Blank, Filled]] (rowsForHint [1, 1] 3)),
+   TestCase (assertEqual "hints: [2, 1], length: 5" [[Filled, Filled, Blank, Filled, Blank],
+                                                     [Filled, Filled, Blank, Blank, Filled],
+                                                     [Blank, Filled, Filled, Blank, Filled]] (rowsForHint [2, 1] 5))
+  ]
+
+runTests = do runTestTT allTests
--- test/puzzle-solver-test.hs
+++
@@ -1,7 +0,0 @@
-import Test.HUnit
-
-test1 = TestCase (assertEqual "2 + 2 should equal 4" (2 + 2) 4)
-
-allTests = TestList [ TestLabel "addition test" test1]
- 
-runTests = do runTestTT allTests
