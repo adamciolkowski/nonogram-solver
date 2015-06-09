6380e43d2dd07cf951e2327a6cfec911abf60dae
Adam Ciolkowski <adamciolkowski1@gmail.com>
2015-06-09 12:31:45 +0200

    Added .cabal file

--- Main.hs
+++ Main.hs
@@ -7,6 +7,7 @@ import PuzzlePrinter
 import PuzzleReader
 
 main = do args <- getArgs
-          if (null args) then error "No path to file given"
-          else do puzzle <- readPuzzle (head args)
-                  printSolution (solvePuzzle puzzle)
+          if (null args)
+            then error "No path to file given"
+            else do puzzle <- readPuzzle (head args)
+                    printSolution (solvePuzzle puzzle)
